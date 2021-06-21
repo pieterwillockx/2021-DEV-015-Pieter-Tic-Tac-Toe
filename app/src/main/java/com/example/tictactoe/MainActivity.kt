35677e778e17e6216e7b5df613f005a1070d9fc7
Pieter Willockx <pieterwillockx2@gmail.com>
2021-06-21 15:00:17 +0200

    - add unit test for testing tictactoe onclick behaviour - add robolectric dependency + update pre-configured dependencies

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.tictactoe
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.view.children
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+
+    private val rowCells : MutableList<TextView> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var cells : ArrayList<TextView> = arrayListOf()
+        cells = Utils.getChildViewsByClass(table_layout, TextView::class.java)
+
+        cells.forEach { it.setOnClickListener {
+                println("cell with name " + it.id + " was clicked!")
+            }
+        }
     }
 
 
