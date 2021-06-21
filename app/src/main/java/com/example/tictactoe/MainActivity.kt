0088505dd8396cbffa29f82f9d324872e2468e77
Pieter Willockx <pieterwillockx2@gmail.com>
2021-06-21 17:14:25 +0200

    add onclick functionality

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : Activity() {
 
     private val rowCells : MutableList<TextView> = mutableListOf()
+    private var playerTurn : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +22,19 @@ class MainActivity : Activity() {
 
         cells.forEach { it.setOnClickListener {
                 println("cell with name " + it.id + " was clicked!")
+                if ((it as TextView).text != "") {
+                    println("cell with name " + it.id + " already has a value!")
+                    return@setOnClickListener
+                }
+
+                if (playerTurn == 0) {
+                    (it as TextView).text = "X"
+                    playerTurn = 1
+                } else if (playerTurn == 1) {
+                    (it as TextView).text = "O"
+                    playerTurn = 0
+                }
             }
         }
     }
-
-
 }
